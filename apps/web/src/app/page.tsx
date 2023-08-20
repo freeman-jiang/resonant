81e34fc6b3e3704f06d1fcc98b0d482c635e1ea6
Freeman Jiang <freeman.jiang.ca@gmail.com>
2023-08-19 23:39:13 -0400

    Increase font size for badges

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -6,15 +6,15 @@ export default function Home() {
   return (
     <div className="p-8 lg:max-w-2xl mx-auto">
       <h1 className="font-bold text-3xl">Superstack</h1>
-      <div className="mt-2 pb-2 flex flex-row gap-2">
-        <Badge className="cursor-pointer">All</Badge>
-        <Badge className="cursor-pointer" variant="outline">
+      <div className="mt-3 pb-2 flex flex-row gap-2">
+        <Badge className="cursor-pointer text-sm">All</Badge>
+        <Badge className="cursor-pointer text-sm" variant="outline">
           Software
         </Badge>
-        <Badge className="cursor-pointer" variant="outline">
+        <Badge className="cursor-pointer text-sm" variant="outline">
           Climate
         </Badge>
-        <Badge className="cursor-pointer" variant="outline">
+        <Badge className="cursor-pointer text-sm" variant="outline">
           Philosophy
         </Badge>
       </div>
