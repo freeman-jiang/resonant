bf1c412fbbc200400b5feaa4a381ed1b8a166fed
Henry Nguyen <henry2833@gmail.com>
2023-08-20 19:11:02 -0700

    scraping updates

--- apps/curation/crawler/crawler.py
+++ apps/curation/crawler/crawler.py
@@ -1,4 +1,5 @@
 import asyncio
+import datetime
 import json
 import multiprocessing
 import re
@@ -7,7 +8,12 @@ from queue import Empty
 from typing import Optional
 from urllib.parse import urlparse, urlunparse
 
+import newspaper
 import trafilatura
+from lxml import etree
+from lxml.html import document_fromstring
+from lxml.html.clean import Cleaner
+from newspaper import Article
 from pydantic import BaseModel, model_validator
 from multiprocessing import Pool, JoinableQueue as Queue
 from aiohttp import ClientSession, ClientError
@@ -15,6 +21,35 @@ from aiohttp import ClientSession, ClientError
 from asyncio import get_event_loop
 from asyncio import ensure_future
 import lmdb
+
+from .root_urls import ROOT_URLS
+
+SUPPRESSED_DOMAINS = [
+    "wikipedia.org", "amazon.com", "youtube.com", "twitter.com", "facebook.com", "reddit.com", "instagram.com",'google.com/patent'
+]
+
+
+def sanitize(dirty_html):
+    cleaner = Cleaner(page_structure=True,
+                      meta=True,
+                      embedded=True,
+                      links=True,
+                      style=True,
+                      processing_instructions=True,
+                      inline_style=True,
+                      scripts=True,
+                      javascript=True,
+                      comments=True,
+                      frames=True,
+                      forms=True,
+                      annoying_tags=True,
+                      remove_unknown_tags=True,
+                      safe_attrs_only=True,
+                      safe_attrs=frozenset(['src', 'color', 'href', 'title', 'class', 'name', 'id']),
+                      remove_tags=('span', 'font', 'div')
+                      )
+
+    return cleaner.clean_html(dirty_html)
 
 
 class Link(BaseModel):
@@ -28,6 +63,7 @@ class Link(BaseModel):
     def validate_url(self):
         if not self.url.startswith("http://") and not self.url.startswith("https://"):
             raise ValueError("Invalid URL: " + self.url)
+
         return self
 
     def domain(self):
@@ -38,11 +74,8 @@ class Link(BaseModel):
         new_url = urlunparse((parsed_url.scheme, parsed_url.netloc, '', '', '', ''))
         return new_url
 
-    def child_link(self, text: str, url: str):
+    def _child_link_inner(self, text: str, url: str):
         if url.startswith("http://") or url.startswith("https://"):
-            return Link(text=text, url=url, parent_url=self.url, depth=self.depth + 1)
-        elif url.startswith("/"):
-            url = self.domain() + url
             return Link(text=text, url=url, parent_url=self.url, depth=self.depth + 1)
         elif url.startswith("#"):
             # Ignore anchor links
@@ -52,9 +85,29 @@ class Link(BaseModel):
             return None
         elif url.endswith(".onion"):
             return None
+        elif url.startswith("//"):
+            url = "http:" + url
+            return Link(text=text, url=url, parent_url=self.url, depth=self.depth + 1)
+        elif url.startswith('/'):
+            url = self.domain() + url
+            return Link(text=text, url=url, parent_url=self.url, depth=self.depth + 1)
         else:
-            print("Invalid URL: " + url + " from parent: " + self.url)
+            url = self.domain() + '/' + url
+            return Link(text=text, url=url, parent_url=self.url, depth=self.depth + 1)
+
+    def child_link(self, text: str, url: str):
+        if url is None :
             return None
+        if text is None:
+            text = ""
+        link = self._child_link_inner(text, url)
+
+        if link is None:
+            return None
+        for suppressed in SUPPRESSED_DOMAINS:
+            if suppressed in link.domain():
+                return None
+        return link
 
 
 class CrawlResult(BaseModel):
@@ -68,7 +121,7 @@ class CrawlResult(BaseModel):
 
 class Database:
     def __init__(self):
-        self.db = lmdb.open("my_database")
+        self.db = lmdb.open("my_database", map_size=int(1e9))
 
     def store(self, key: str, value: CrawlResult):
         with self.db.begin(write=True) as txn:
@@ -121,12 +174,45 @@ global_state: GlobalState = GlobalState()
 MAX_DEPTH = 4
 
 
-def parse_html(html: str, url: Link) -> Optional[CrawlResult]:
+def parse_html_newspaper(html: str, url: Link) -> Optional[CrawlResult]:
+    article = newspaper.Article(url.url, keep_article_html=True)
+    try:
+        article.download()
+        article.parse()
+    except newspaper.ArticleException:
+        return None
+
+    tree = article.clean_top_node
+
+    if tree is None:
+        return None
+    # Extract all <a> tags
+    link_elements = tree.xpath('//a')
+
+    # Extract href attribute and link text from each <a> tag
+    links = [url.child_link(element.text, element.get('href')) for element in link_elements]
+    links = filter(lambda k: k is not None, links)
+
+    publish_date = article.publish_date.isoformat() if article.publish_date is not None else None
+
+
+    return CrawlResult(
+        url=url,
+        title=article.title,
+        date=publish_date,
+        author=str(article.authors),
+        content=article.text,
+        outgoing_links=list(links)
+    )
+
+
+def parse_html_trafilatura(html: str, url: Link) -> Optional[CrawlResult]:
+    # html = sanitize(html)
+    html += "</body></html>"
     content = trafilatura.extract(html, url=url.url, include_links=True, include_tables=True, output_format='json',
                                   with_metadata=True)
 
     if content is None:
-        print("Failed to parse: " + url.url)
         return None
     content = json.loads(content)
 
@@ -140,6 +226,15 @@ def parse_html(html: str, url: Link) -> Optional[CrawlResult]:
         content=content['text'],
         outgoing_links=links
     )
+
+
+def parse_html(html: str, url: Link) -> Optional[CrawlResult]:
+    a = parse_html_trafilatura(html, url)
+    if a is None:
+        a = parse_html_newspaper(html, url)
+        if a is None:
+            print("Failed to parse: " + url.url + " from" + url.parent_url)
+    return a
 
 
 def extract_links_from_markdown(markdown_text: str, parent: Link) -> list[Link]:
@@ -162,7 +257,8 @@ async def query_internal(url: Link, session, queue: Queue):
     async with session.get(url.url) as response:
         response = await response.read()
 
-        result = parse_html(response, url)
+
+        result = parse_html(response.decode('utf-8', errors='ignore'), url)
 
         if result is None:
             return
@@ -218,6 +314,7 @@ async def run(url_queue: Queue):
                 if f.exception():
                     f.print_stack()
                     f.result()
+                    global_state.done_flag.set()
                 url_queue.task_done()
 
     backoff = ExponentialBackoff()
@@ -270,10 +367,11 @@ def set_work_queue(global_state_new: GlobalState):
 def main():
     global global_state
 
-    global_state.work_queue.put(
-        Link(text="Polymath Playbook", url="http://archagon.net/blog/2018/03/24/data-laced-with-history/", parent_url="root"))
+    for root in ROOT_URLS:
+        global_state.work_queue.put(
+            Link(text=root["title"], url=root["url"], parent_url="root"))
 
-    num_workers = 1
+    num_workers = 3
     with Pool(num_workers, set_work_queue, (global_state,)) as pool:
         _workers = [pool.apply_async(worker_main) for _ in range(num_workers)]
 
@@ -290,20 +388,14 @@ def main():
         pool.join()
 
 
-import cfscraper
+import trafilatura.external
+import logging
 
+# logging.basicConfig(level=logging.ERROR)
 if __name__ == "__main__":
+
+    # import cfscrape
     # scraper = cfscraper.create_scraper()  # returns a CloudflareScraper instance
     # # Or: scraper = cfscrape.CloudflareScraper()  # CloudflareScraper inherits from requests.Session
     # print(scraper.get("https://carta.com/blog/carta-101/").content)
     main()
-
-# async def crawl(url: str) -> CrawlResult:
-#     data = req
-#
-# def start_crawl(root_urls = None):
-#     if root_urls is None:
-#         root_urls = ROOT_URLS
-#
-#     for root_url in root_urls:
-#         crawl(root_url)
