f813b4b0a2df750dc2a7b79679241c8622a07727
Freeman Jiang <freeman.jiang.ca@gmail.com>
2023-09-18 15:29:33 -0400

    web: add topics, improve UX

--- apps/web/src/components/Topics.tsx
+++ apps/web/src/components/Topics.tsx
@@ -1,33 +1,49 @@
 import { fetchFeed, searchFor } from "@/api";
 import { useFeed } from "@/context/FeedContext";
+import { useState } from "react";
 import { Badge } from "./ui/badge";
 
-const TOPICS = ["Software", "Climate", "Philosophy"];
+enum Topic {
+  Software,
+  Climate,
+  Philosophy,
+  Politics,
+  Science,
+}
+
+const TOPIC_LIST = Object.keys(Topic).filter((key) => isNaN(Number(key))); // filter out the numeric keys
+const ALL = "All";
 
 export const Topics = () => {
   const { setLinks } = useFeed();
+  const [currentTopic, setCurrentTopic] = useState<string>(ALL);
   const searchTopic = async (topic: string) => {
     const data = await searchFor(topic);
     setLinks(data);
   };
 
   return (
-    <div className="mt-3 flex flex-row gap-2 pb-2">
+    <div className="mt-3 flex flex-row flex-wrap gap-2 pb-2">
       <Badge
         className="cursor-pointer text-sm"
         onClick={async () => {
           const data = await fetchFeed();
           setLinks(data);
+          setCurrentTopic(ALL);
         }}
+        variant={currentTopic === ALL ? "default" : "outline"}
       >
         All
       </Badge>
-      {TOPICS.map((topic) => (
+      {TOPIC_LIST.map((topic) => (
         <Badge
           key={topic}
           className="cursor-pointer text-sm"
-          variant="outline"
-          onClick={() => searchTopic(topic)}
+          variant={currentTopic === topic ? "default" : "outline"}
+          onClick={async () => {
+            await searchTopic(topic);
+            setCurrentTopic(topic);
+          }}
         >
           {topic}
         </Badge>
