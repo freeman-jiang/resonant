b7c8f57efc1d2ace28d6edd83febeaef6b0356ab
Henry Nguyen <henry2833@gmail.com>
2023-09-27 15:58:13 -0400

    crawler: add HN rss feed

---
+++ apps/backend/crawler/manual/add_recent_hn_urls.py
@@ -0,0 +1,31 @@
+import asyncio
+
+import requests
+from prisma import Prisma
+
+from crawler.config import Config
+from crawler.link import Link
+from crawler.parse import find_feed_urls_cached
+from crawler.prismac import PrismaClient
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# Get stories >= 80 points and 30 comments
+rssfeed = 'https://hnrss.org/newest?points=80&comments=30&count=100'
+
+async def get_rss_feed():
+    config = Config()
+
+    db = Prisma()
+    await db.connect()
+    pc = PrismaClient(config, db)
+
+    links = find_feed_urls_cached(Link.from_url(rssfeed))
+
+    await pc.add_tasks(links)
+
+
+if __name__ == "__main__":
+    asyncio.run(get_rss_feed())
