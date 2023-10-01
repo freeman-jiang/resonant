b0ea3248361034d21a644bf73ad3220883a73b2c
Freeman Jiang <freeman.jiang.ca@gmail.com>
2023-10-01 01:28:44 -0400

    web: force dynamic

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -3,6 +3,8 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default async function AuthenticationPage() {
   const supabase = createServerComponentClient({ cookies });
   const {
