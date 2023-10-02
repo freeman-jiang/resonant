1f564a2979fbd45eb514866b687b59af13560bf6
Freeman Jiang <freeman.jiang.ca@gmail.com>
2023-10-02 17:15:01 -0400

    web: actually fix auth callback

--- apps/web/src/app/auth/callback/route.ts
+++ apps/web/src/app/auth/callback/route.ts
@@ -1,7 +1,6 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
-import { getUser } from "./../../../api/index";
 
 import type { NextRequest } from "next/server";
 
@@ -15,14 +14,7 @@ export async function GET(request: NextRequest) {
   if (code) {
     const supabase = createRouteHandlerClient({ cookies });
     await supabase.auth.exchangeCodeForSession(code);
-
-    const {
-      data: { user: supabaseUser },
-    } = await supabase.auth.getUser();
-    const user = await getUser(supabaseUser.id);
-    if (!user) {
-      return NextResponse.redirect(`${requestUrl.origin}/new`);
-    }
   }
+
   return NextResponse.redirect(requestUrl.origin);
 }
