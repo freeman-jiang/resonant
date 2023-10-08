5c0f44b9c066952382b4a200627e88d329a0fe32
Freeman Jiang <freeman.jiang.ca@gmail.com>
2023-10-08 16:00:26 -0400

    web: deduplicate senders

--- apps/web/src/components/Entry.tsx
+++ apps/web/src/components/Entry.tsx
@@ -21,28 +21,42 @@ export const Entry = (page: Page) => {
 
   // TODO: Limit number of avatars shown to have +X more
   const renderAvatars = () => {
-    const avatars = senders.map((sender) => {
-      const initials = `${sender.first_name[0]}${sender.last_name[0]}`;
+    // Filter out duplicate senders
+    // TODO: This should maybe probably be done on backend
+    const senderIds = new Set();
 
-      return (
-        <TooltipProvider delayDuration={0} key={sender.id}>
-          <Tooltip>
-            <TooltipTrigger className="cursor-default">
-              <Avatar className="h-6 w-6">
-                <AvatarImage src={sender.profile_picture_url} />
-                <AvatarFallback className="text-xs">{initials}</AvatarFallback>
-              </Avatar>
-            </TooltipTrigger>
-            <TooltipContent>
-              <div className="text-slate-800">{`${sender.first_name} ${sender.last_name}`}</div>
-              <div className="text-xs text-slate-500">
-                {getRelativeTime(sender.sent_on)}
-              </div>
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-      );
-    });
+    const avatars = senders
+      .filter((sender) => {
+        if (senderIds.has(sender.id)) {
+          return false;
+        }
+        senderIds.add(sender.id);
+        return true;
+      })
+      .map((sender) => {
+        const initials = `${sender.first_name[0]}${sender.last_name[0]}`;
+
+        return (
+          <TooltipProvider delayDuration={0} key={sender.id}>
+            <Tooltip>
+              <TooltipTrigger className="cursor-default">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src={sender.profile_picture_url} />
+                  <AvatarFallback className="text-xs">
+                    {initials}
+                  </AvatarFallback>
+                </Avatar>
+              </TooltipTrigger>
+              <TooltipContent>
+                <div className="text-slate-800">{`${sender.first_name} ${sender.last_name}`}</div>
+                <div className="text-xs text-slate-500">
+                  {getRelativeTime(sender.sent_on)}
+                </div>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        );
+      });
 
     return <div className="flex -space-x-1.5">{avatars}</div>;
   };
