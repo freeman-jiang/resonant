0222954c726329c1bea2229e1f13890dde09cf74
Freeman Jiang <freeman.jiang.ca@gmail.com>
2024-01-17 18:49:30 -0500

    web: add empty for you state

--- apps/web/src/components/feeds/ForYouFeed.tsx
+++ apps/web/src/components/feeds/ForYouFeed.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useForYou } from "@/api/hooks";
+import { ZapOff } from "lucide-react";
 import { Feed, LoadingFeed } from "../Feed";
 
 interface Props {
@@ -11,6 +12,18 @@ export const ForYouFeed = ({ userId }: Props) => {
 
   if (isLoading) {
     return <LoadingFeed />;
+  }
+
+  if (data.length === 0) {
+    return (
+      <div className="flex flex-col items-center">
+        <ZapOff className="mt-6 block h-10 w-10 text-slate-600" />
+        <div className="mt-8 text-center text-base text-slate-600">
+          <div>{"Looks like there's nothing here :("}</div>
+          <div>Try liking some articles to get recommendations.</div>
+        </div>
+      </div>
+    );
   }
 
   return (
